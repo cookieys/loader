8e0b5af36b5d0b5c39db9697d37a5e1160cfde56
XryaV <cookieys@hotmail.com>
2025-03-29 18:45:33 +0700

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,6 @@
+local Games = {
+    [8540346411] = "https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/Rebirth.lua",
+    [16732694052] = "https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Fisch.lua",
+}
+
+return Games
