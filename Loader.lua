69f1a8a371828b783de53952669e25ffde5fb4f1
XryaV <cookieys@hotmail.com>
2025-03-29 18:44:52 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+--[[
+
+discord.gg/25ms
+
+--]]
+local s="https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Game%20list.lua"local U,D=pcall(function()return(loadstring(game:HttpGet(s)))()end)if U and D then local s=game.PlaceId if D[s]then local U,v=pcall(function()return(loadstring(game:HttpGet(D[s])))()end)if not U then print("Error loading game script:",v)game.Players.LocalPlayer:Kick("Error loading game script.")end else print("Game not supported. Kicking player.")game.Players.LocalPlayer:Kick("This game is not supported by the script.")end else print("Error loading games list:",D)end
