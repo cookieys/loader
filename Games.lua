d37a69727c152c90432cd07ffd3626381c7cd029
XryaV <cookieys@hotmail.com>
2025-03-30 07:39:40 +0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,6 +1,18 @@
+--[[
+    Game Configuration:
+    Maps Roblox Place IDs to their corresponding script URLs.
+    The loader script will use this table to find and execute the correct
+    script based on the current game's Place ID.
+]]
 local Games = {
+    -- Rebirth Champion X
     [8540346411] = "https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/Rebirth.lua",
+
+    -- Fisch
     [16732694052] = "https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Fisch.lua",
+
+    -- Add new games below:
+    -- [PLACE_ID_HERE] = "URL_TO_SCRIPT_HERE.lua",
 }
 
 return Games
