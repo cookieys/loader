4098347d374d58527fc72044ef26b1d66d56816f
XryaV <cookieys@hotmail.com>
2025-04-09 15:48:20 +0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -6,7 +6,7 @@
 ]]
 local Games = {
     -- Rebirth Champion X
-    [8540346411] = "https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/Rebirth.lua",
+    [8540346411] = "https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Rcx.lua",
 
     -- Fisch
     [16732694052] = "https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Fisch.lua",
